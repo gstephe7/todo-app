963f2bc38f44c636d72e76878a4ac7a6349d98dc
Greg <gstephe7@gmail.com>
2017-06-10 09:26:21 -0400

    split individual list items into components

--- todo/src/todo-list.vue
+++ todo/src/todo-list.vue
@@ -1,15 +1,12 @@
 <template>
 
-  <!-- List Item -->
+  <!-- List -->
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
         <ul>
-          <li class="todo-item" v-for="(item, index) in list">
-            <i class="fa fa-check mark-complete" title="Mark Complete" v-on:click="markComplete()"></i>
-            <i class="fa fa-exclamation mark-priority" title="Set as Priority" v-on:click="markPriority()"></i>
-            <i class="fa fa-times clear" title="Remove Task" v-on:click="() => list.splice(index, 1)"></i>
-            <p v-text="item" v-bind:class="{ complete : complete, priority : priority }"></p>
+          <li v-for="item in list">
+            <list-item v-bind:list="list"></list-item>
           </li>
         </ul>
       </div>
@@ -19,61 +16,16 @@
 </template>
 
 <script>
-export default {
-  props: ['list'],
-  data: function () {
-    return {
-      complete: false,
-      priority: false
-    }
-  },
-  methods: {
-    markComplete: function () {
-      this.complete = !this.complete
-      this.priority = false
-    },
-    markPriority: function () {
-      if (this.complete === false) {
-        this.priority = !this.priority
-      }
-    }
+  import Vue from 'vue'
+  import ListItem from './todo-listitem.vue'
+
+  Vue.component('list-item', ListItem)
+
+  export default {
+    props: ['list']
   }
-}
 </script>
 
 <style>
-  li {
-    list-style-type: none;
-  }
 
-  .todo-item {
-    border: thin lightgrey solid;
-    border-radius: 10px;
-    margin-top: 5px;
-  }
-
-  .clear {
-    color: red;
-    cursor: pointer;
-  }
-
-  .mark-complete {
-    color: green;
-    cursor: pointer;
-  }
-
-  .mark-priority {
-    color: orange;
-    cursor: pointer;
-  }
-
-  .complete {
-    text-decoration: line-through;
-    color: rgba(84, 198, 73, 1);
-  }
-
-  .priority {
-    text-decoration: underline;
-    font-weight: bold;
-  }
 </style>
---
+++ todo/src/todo-listitem.vue
@@ -0,0 +1,71 @@
+<template>
+
+  <!-- List Item -->
+  <div>
+    <i class="fa fa-check mark-complete" title="Mark Complete" v-on:click="markComplete()"></i>
+    <i class="fa fa-exclamation mark-priority" title="Set as Priority" v-on:click="markPriority()"></i>
+    <i class="fa fa-times clear" title="Remove Task" v-on:click="() => list.splice(index, 1)"></i>
+    <p v-text="item" v-bind:class="{ complete : complete, priority : priority }"></p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['list'],
+  data: function () {
+    return {
+      complete: false,
+      priority: false
+    }
+  },
+  methods: {
+    markComplete: function () {
+      this.complete = !this.complete
+      this.priority = false
+    },
+    markPriority: function () {
+      if (this.complete === false) {
+        this.priority = !this.priority
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  li {
+    list-style-type: none;
+  }
+
+  .todo-item {
+    border: thin lightgrey solid;
+    border-radius: 10px;
+    margin-top: 5px;
+  }
+
+  .clear {
+    color: red;
+    cursor: pointer;
+  }
+
+  .mark-complete {
+    color: green;
+    cursor: pointer;
+  }
+
+  .mark-priority {
+    color: orange;
+    cursor: pointer;
+  }
+
+  .complete {
+    text-decoration: line-through;
+    color: rgba(84, 198, 73, 1);
+  }
+
+  .priority {
+    text-decoration: underline;
+    font-weight: bold;
+  }
+</style>
